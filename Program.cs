58d8e5ad3fe28afcca2288b00e6539d4215c9c6b
Irina632015 <irina.632015@yandex.ru>
2023-01-27 20:01:41 +0400

    Семинар 3 Задача 23

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+int num = ReadInt("Введите число N: ");
+for (int i = 1; i <= num; i++)
+{ 
+    Console.Write($"{i*i*i} ");
+}
+int ReadInt(string msg)
+{
+    Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
